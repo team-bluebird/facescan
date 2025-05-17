583569b76205a30754b7c7de3b9af3da51b2f9d8
jungdadak <junghyun21110@gmail.com>
2025-05-17 20:13:28 +0900

    refactor: 버튼 컴포넌트, 시멘틱 태그 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,37 @@
 import Image from "next/image";
 
+import MainBtn from "@/components/btn/MainBtn";
+
 export default function Home() {
   return (
-    <div>
+    <main>
       <img
         src="/bg/main.png"
-        alt="배경"
-        className="absolute inset-0 max-w-xl object-contain mx-auto h-full min-h-[813] -z-10"
+        alt=""
+        className="absolute inset-0 -z-10 mx-auto h-full min-h-[813px] max-w-xl object-contain"
       />
 
       <h1
-        className={`w-[304px] h-[78px] font-bold text-[28px] mx-auto text-center mt-40 tracking-[-0.56px] leading-[39.2px]`}
+        className={`mx-auto mt-40 h-[78px] w-[304px] text-center text-[28px] leading-[39.2px] font-bold tracking-[-0.56px]`}
       >
         나의 표정으로
         <br />
         감정을 분석하고 싶다면?
       </h1>
 
-      <div
+      <figure
         className={
-          "flex justify-center items-center bg-white/10 w-[234px] h-[280px] rounded-4xl mt-16 mx-auto border-white/10 border-2"
+          "mx-auto mt-16 flex h-[280px] w-[234px] items-center justify-center rounded-4xl border-2 border-white/10 bg-white/10"
         }
+        aria-labelledby="question-caption"
       >
-        <Image src={"/icon/question.png"} alt={"?"} width={126} height={140} />
-      </div>
+        <Image src={"/icon/question.png"} alt={""} width={126} height={140} />
+        <figcaption id="question-caption" className="sr-only">
+          귀여운 물음표 아이콘
+        </figcaption>
+      </figure>
 
-      <button
-        className={`mt-20 w-[304px] h-[50px] flex justify-center items-center gap-2 px-8 py-4 mx-auto bg-[#8D56FF]/80 rounded-4xl`}
-      >
-        <p
-          className={
-            "font-semibold text-lg text-center leading-none tracking-0"
-          }
-        >
-          테스트 하기
-        </p>
-      </button>
-    </div>
+      <MainBtn>테스트하기</MainBtn>
+    </main>
   );
 }
