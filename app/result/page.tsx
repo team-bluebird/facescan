756644572b767e5cc1b31a5ab0bf1f368e9d3297
jungdadak <junghyun21110@gmail.com>
2025-05-17 14:41:13 +0900

    style: main BG & Icon

---
+++ app/result/page.tsx
@@ -0,0 +1,11 @@
+export default function Result() {
+  return (
+    <div>
+      <img
+        src="/bg/sub.png"
+        alt="배경2"
+        className="absolute inset-0 w-full h-full object-contain -z-10"
+      />
+    </div>
+  );
+}
