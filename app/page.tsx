f0e4629124967878ddad6265b667b1cf8255e246
jungdadak <junghyun21110@gmail.com>
2025-05-17 19:33:06 +0900

    fix: Pretendard 폰트 설정

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ export default function Home() {
       <img
         src="/bg/main.png"
         alt="배경"
-        className="absolute inset-0 w-full h-full -z-10"
+        className="absolute inset-0 max-w-xl object-contain mx-auto h-full min-h-[813] -z-10"
       />
 
-      <h1 className={`font-bold text-[28px] text-center mt-40 leading-snug`}>
+      <h1
+        className={`w-[304px] h-[78px] font-bold text-[28px] mx-auto text-center mt-40 tracking-[-0.56px] leading-[39.2px]`}
+      >
         나의 표정으로
         <br />
         감정을 분석하고 싶다면?
@@ -24,7 +26,7 @@ export default function Home() {
       </div>
 
       <button
-        className={`block mt-20 w-[304px] h-[50px] mx-auto bg-[#8D56FF]/80 rounded-4xl`}
+        className={`mt-20 w-[304px] h-[50px] flex justify-center items-center gap-2 px-8 py-4 mx-auto bg-[#8D56FF]/80 rounded-4xl`}
       >
         <p
           className={
